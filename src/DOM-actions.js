c07c7fdac7a024200be1a8d412121ba3bb16015e
Francois <francois.thullier98@gmail.com>
2024-12-12 11:17:43 +0100

    Added getCellId, randomizeShips, and setCellImage functions Rearanged startGame such that it calls renderBoard when the game starts

--- src/DOM-actions.js
+++ src/DOM-actions.js
@@ -1,8 +1,49 @@
-const addPlayerName = (() => {
+import { Ship } from "./ship.js";
+import missImg from "./img/icons8-red-cross-48.png";
+
+const renderGameboard = (gameboard, gameboardContainer) => {
+  const board = gameboard.getBoard();
+
+  //functions
+  const createBoard = (container) => {
+    container.style.visibility = "";
+    for (let i = 1; i < board.length + 1; i++) {
+      for (let j = 1; j < board.length + 1; j++) {
+        const cell = document.createElement("div");
+        cell.setAttribute("class", `${container.className}-cell`);
+        cell.setAttribute("id", `${i}-${j}`);
+
+        container.appendChild(cell);
+      }
+    }
+  };
+
+  const getCellId = (cell) => {
+    //should return attackX and attackY coordinates
+    cell.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        const cellX = `${cell.className}-${cell.id}`.split("-")[3];
+        const cellY = `${cell.className}-${cell.id}`.split("-")[4];
+        setCellImage(cell, missImg);
+
+        console.log(cellX, cellY);
+        return { cellX, cellY };
+      });
+    });
+  };
+
+  //function call
+  createBoard(gameboardContainer);
+  getCellId(document.querySelectorAll(`.${gameboardContainer.className}-cell`));
+};
+
+const startGame = (playerGameboard, computerGameboard) => {
+  const playerGameboardContainer = document.querySelector(".player-board");
+  const computerGameboardContainer = document.querySelector(".computer-board");
+
   const startGameButton = document.getElementById("start-game");
   const playerNameInput = document.getElementById("player-name");
   const playerInputForm = document.querySelector(".player-name-input");
-  const mainContainer = document.querySelector(".container");
 
   startGameButton.addEventListener("click", () => {
     const acceptName = document.getElementById("select-name-input");
@@ -12,8 +53,10 @@ const addPlayerName = (() => {
     acceptName.addEventListener("click", () => {
       let nameValue = playerNameInput.value;
       displayPlayerName(nameValue);
-      console.log(nameValue);
       playerInputForm.style.visibility = "hidden";
+
+      renderGameboard(playerGameboard, playerGameboardContainer);
+      renderGameboard(computerGameboard, computerGameboardContainer);
     });
     cancelNameSelection.addEventListener("click", () => {
       playerInputForm.style.visibility = "hidden";
@@ -24,25 +67,26 @@ const addPlayerName = (() => {
     const playerNameDisplay = document.querySelector(".player-one >h2");
     playerNameDisplay.textContent = name;
   };
-})();
-
-const renderGameboard = (gameboard) => {
-  const board = gameboard.getBoard();
-  console.log(board);
-
-  const createBoard = (() => {
-    const boardContainer = document.querySelector(".board");
-    console.log(boardContainer);
-    for (let i = 1; i < board.length + 1; i++) {
-      for (let j = 1; j < board.length + 1; j++) {
-        const cell = document.createElement("div");
-        cell.setAttribute("class", "cell");
-        cell.setAttribute("id", `${i}-${j}`);
-
-        boardContainer.appendChild(cell);
-      }
-    }
-  })();
 };
 
-export { addPlayerName, renderGameboard };
+const setAttackMarker = (state) => {
+  //state will be 1 if attack hits
+  // and 0 if attack misses
+};
+
+const randomizeShips = () => {
+  let ships = [];
+  for (let i = 0; i < 5; i++) {
+    ships.push(Ship(Math.floor(Math.random() * 4 + 2), 0, false));
+  }
+  //returns an array of 5 ships with random length
+  return ships;
+};
+
+const setCellImage = (cell, img) => {
+  const imageContainer = document.createElement("img");
+  imageContainer.src = img;
+
+  cell.appendChild(imageContainer);
+};
+export { startGame, renderGameboard, randomizeShips, setCellImage };
