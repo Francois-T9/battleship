68e5fc2b3e6a1aa0638eb264ea8923a967034388
Francois <francois.thullier98@gmail.com>
2024-12-16 13:14:09 +0100

    Changed file name and added clearBoard function

---
+++ src/dom.js
@@ -0,0 +1,76 @@
+const setAttackMarker = (state) => {
+  //state will be 1 if attack hits
+  // and 0 if attack misses
+};
+
+const randomizeShips = () => {
+  let ships = [];
+  let shipsPosition = [];
+
+  for (let i = 0; i < 5; i++) {
+    // Create a ship with random length between 2 and 5
+    let length = Math.floor(Math.random() * 4 + 2);
+    let orientation = Math.random() < 0.5 ? "horizontal" : "vertical";
+
+    let position;
+    let validPlacement = false;
+
+    while (!validPlacement) {
+      // Generate random starting position
+      let row = Math.floor(Math.random() * 10);
+      let col = Math.floor(Math.random() * 10);
+
+      // Check if the ship fits within the board based on orientation
+      if (
+        (orientation === "horizontal" && col + length <= 10) ||
+        (orientation === "vertical" && row + length <= 10)
+      ) {
+        position = { row, col, orientation };
+        validPlacement = true;
+      }
+    }
+
+    ships.push({ length, orientation });
+    shipsPosition.push(position);
+  }
+
+  return { ships, shipsPosition };
+};
+
+const displayShips = (gameboard, ships, position) => {
+  const board = gameboard.getBoard();
+
+  for (let i = 0; i < ships.length; i++) {
+    gameboard.setShip(
+      board,
+      ships[i],
+
+      position[i].row,
+      position[i].col
+    );
+  }
+};
+
+const setCellImage = (cell, img) => {
+  const imageContainer = document.createElement("img");
+  imageContainer.src = img;
+
+  cell.appendChild(imageContainer);
+};
+
+const clearBoard = (boardContainer, boardObject) => {
+  boardContainer.innerHTML = "";
+  for (let i = 0; i < boardObject.length; i++) {
+    for (let j = 0; j < boardObject.length; j++) {
+      boardObject[i][j] = 0;
+    }
+  }
+};
+
+export {
+  clearBoard,
+  displayShips,
+  setAttackMarker,
+  randomizeShips,
+  setCellImage,
+};
