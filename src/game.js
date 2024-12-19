8d1a790034d85df2e784c39dd4dbdcc06b30387a
Francois <francois.thullier98@gmail.com>
2024-12-19 12:01:34 +0100

    Reworked the whole getCellId function, work still in progress

--- src/game.js
+++ src/game.js
@@ -9,49 +9,26 @@ const game = (
   playerBoardObject,
   computerBoardObject
 ) => {
-  const playerTurn = false;
-  const computerTurn = false;
-
   const playerBoard = playerBoardObject.getBoard();
   const playerShips = playerBoardObject.ships;
 
-  let playerMissedHits = 0;
-
   const getCellId = (cell) => {
-    //should return attackX and attackY coordinates
+    // Should return attackX and attackY coordinates
     cell.forEach((cell) => {
       cell.addEventListener("click", () => {
         const cellX = `${cell.className}-${cell.id}`.split("-")[3] - 1;
         const cellY = `${cell.className}-${cell.id}`.split("-")[4] - 1;
 
-        let playerHit = false;
-        playerShips.forEach((ship) => {
-          //ships needs to have a "position" argument so we know
-          //which ship has been hit
-          if (
-            playerBoardObject.receiveAttack(playerBoard, ship, cellX, cellY)
-          ) {
-            playerHit = true;
-            setCellImage(cell, hitImg);
-            console.log(
-              "Player attacks cell : ",
-              `${cellX}-${cellY}`,
-              "and hits",
-              `${ship.orientation}`,
-              "ship of length ",
-              `${ship.length}`
-            );
-          }
-        });
-        if (!playerHit) {
-          playerMissedHits++;
-          setCellImage(cell, missImg);
-        }
-        console.log(playerShips);
+        console.log("Player attacks cell: ", `${cellX}-${cellY}`);
+
+        playerBoardObject.receiveAttack(cellX, cellY);
+        // console.log(playerBoardObject.getBoard());
+        // console.log(playerShips);
         return { cellX, cellY };
       });
     });
   };
+
   getCellId(document.querySelectorAll(`.${playerGameboard.className}-cell`));
 };
 export { game };
